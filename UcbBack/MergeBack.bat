6ea6d994ff1d069d12c55a45199be67b9ddd09e0
vafnucb <vafnucb2020@gmail.com>
2023-12-21 15:23:41 -0400

    Mejorando script

--- UcbBack/MergeBack.bat
+++ UcbBack/MergeBack.bat
@@ -1,9 +1,9 @@
 @echo off
 
 REM Archivos específicos a excluir durante el merge (con rutas relativas)
-EXCLUDED_FILE_1=Web.config
-EXCLUDED_FILE_2=App_Start\WebApiConfig.cs
-EXCLUDED_FILE_3=Controllers\OptionsController.cs
+set EXCLUDED_FILE_1=Web.config
+set EXCLUDED_FILE_2=App_Start\WebApiConfig.cs
+set EXCLUDED_FILE_3=Controllers\OptionsController.cs
 
 REM Realizar un stash de los cambios locales en la rama de destino (main)
 git stash
@@ -17,7 +17,7 @@ git checkout --ours -- "%EXCLUDED_FILE_2%"
 git checkout --ours -- "%EXCLUDED_FILE_3%"
 
 REM Commit de los cambios en la rama de destino (main)
-git add "%EXCLUDED_FILE_1%" "%EXCLUDED_FILE_2%" "%EXCLUDED_FILE_3%"
+git add .
 git commit -m "Merge de devBack en main, excluyendo cambios en %EXCLUDED_FILE_1%, %EXCLUDED_FILE_2%, %EXCLUDED_FILE_3%"
 
 REM Aplicar los cambios locales nuevamente (sin aplicar los cambios de los archivos específicos)
